3a8da4c7df5a24c2ef0dc5255030cc6b09c9edeb
Steve Sheets <ssheets@wareto.com>
2017-10-11 17:53:40 -0400

    Initial Commit

---
+++ NearByAR/NearByAR.h
@@ -0,0 +1,19 @@
+//
+//  NearByAR.h
+//  NearByAR
+//
+//  Created by Steve Sheets on 10/11/17.
+//  Copyright © 2017 Zodiac Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NearByAR.
+FOUNDATION_EXPORT double NearByARVersionNumber;
+
+//! Project version string for NearByAR.
+FOUNDATION_EXPORT const unsigned char NearByARVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NearByAR/PublicHeader.h>
+
+
