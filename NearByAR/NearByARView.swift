3a662b5a75943aa17771eef0f89e5e991a3bc1c2
Steve Sheets <ssheets@wareto.com>
2017-10-11 17:54:19 -0400

    Add NearByAR View

---
+++ NearByAR/NearByARView.swift
@@ -0,0 +1,21 @@
+//
+//  NearByARView.swift
+//  NearByAR
+//
+//  Created by Steve Sheets on 10/11/17.
+//  Copyright © 2017 Zodiac Innovations. All rights reserved.
+//
+
+import UIKit
+
+class NearByARView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
